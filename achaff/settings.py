68668fc5bac2e67ec55f23f64cf45992d920ec67
Brian Ndikum <ntschangb@yahoo.com>
2019-09-19 19:29:13 +0100

    'added_local_settings'

--- achaff/settings.py
+++ achaff/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'wgrealestate21@gmail.com'
 EMAIL_HOST_PASSWORD = 'wgRealEstate21'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
