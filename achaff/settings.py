a30b6f5d3d125ecc4113e062bfdd7001060b81cb
Brian Ndikum <ntschangb@yahoo.com>
2019-09-19 21:21:33 +0100

    'local_settings'

--- achaff/settings.py
+++ achaff/settings.py
@@ -145,6 +145,13 @@ MESSAGE_TAGS = {
     
 }
 
+#Email  config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'wgrealestate21@gmail.com'
+EMAIL_HOST_PASSWORD = 'wgRealEstate21'
+EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
